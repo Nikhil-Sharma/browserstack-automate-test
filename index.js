ced159932fbe5242b0b26ee4d2ecdc9d2285e59a
Nikhil Sharma <sharma.k.nikhil@gmail.com>
2021-01-04 15:43:42 +0530

    adding variable data

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var webdriver = require('selenium-webdriver');
 const{Builder, By, Key, util} = require('selenium-webdriver');
+
+var username = process.env.BROWSERSTACK_USERNAME;
+var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+var buildName = process.env.BROWSERSTACK_BUILD_NAME;
+var browserstackLocal = process.env.BROWSERSTACK_LOCAL;
+var browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
+
 // Input capabilities
 var capabilities = {
  'os_version' : '10',
@@ -7,10 +14,11 @@ var capabilities = {
  'browserName' : 'Chrome',
  'browser_version' : 'latest',
  'os' : 'Windows',
- 'name': 'test 101', // test name
- 'build': 'BROWSERSTACK_BUILD_NAME', // CI/CD job or build name
- 'browserstack.user' : 'BROWSERSTACK_USERNAME',
- 'browserstack.key' : 'BROWSERSTACK_ACCESS_KEY'
+ "build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+ "browserstack.local" : browserstackLocal,
+ "browserstack.localIdentifier" : browserstackLocalIdentifier,
+ "browserstack.user" : username,
+ "browserstack.key" : accessKey
 }
 
 var driver = new webdriver.Builder().usingServer('https://hub-cloud.browserstack.com/wd/hub').
