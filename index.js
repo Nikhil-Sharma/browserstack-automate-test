b028030ceb884ddf19a433eb3662f90259a37408
Nikhil Sharma <sharma.k.nikhil@gmail.com>
2021-01-06 12:59:48 +0530

    fix

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+var webdriver = require('selenium-webdriver');
+
 var username = process.env.BROWSERSTACK_USERNAME;
 var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
 var buildName = process.env.BROWSERSTACK_BUILD_NAME;
 var browserstackLocal = process.env.BROWSERSTACK_LOCAL;
 var browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
-var webdriver = require('selenium-webdriver');
+
 
 
 var capabilities = {
-	"os" : "Windows",
-	"os_version" : "10",
-	"browser" : "chrome",
-	"browser_version" : "latest",
+	'os_version' : '10',
+	'resolution' : '1920x1080',
+	'browserName' : 'Chrome',
+	'browser_version' : 'latest',
+	'os' : 'Windows',
 	"name": "BStack -[Jenkins] Localtesting", // test name
 	"build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
 	"browserstack.local" : browserstackLocal,
