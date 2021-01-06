161b2e5ba18e26b357d8dbc3727f450ce2d83dfe
Nikhil Sharma <sharma.k.nikhil@gmail.com>
2021-01-06 12:36:07 +0530

    Local testing

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-var webdriver = require('selenium-webdriver');
-const{Builder, By, Key, util} = require('selenium-webdriver');
-
 var username = process.env.BROWSERSTACK_USERNAME;
 var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
 var buildName = process.env.BROWSERSTACK_BUILD_NAME;
 var browserstackLocal = process.env.BROWSERSTACK_LOCAL;
 var browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
 
-// Input capabilities
 var capabilities = {
- 'os_version' : '10',
- 'resolution' : '1920x1080',
- 'browserName' : 'Chrome',
- 'browser_version' : 'latest',
- 'os' : 'Windows',
- 'name': 'test Jenkins', // test name
- "build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
- "browserstack.local" : browserstackLocal,
- "browserstack.localIdentifier" : browserstackLocalIdentifier,
- "browserstack.user" : username,
- "browserstack.key" : accessKey
-}
+	"os" : "Windows",
+	"os_version" : "10",
+	"browser" : "chrome",
+	"browser_version" : "latest",
+	"name": "BStack -[Jenkins] Localtesting", // test name
+	"build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+	"browserstack.local" : browserstackLocal,
+	"browserstack.localIdentifier" : browserstackLocalIdentifier,
+	"browserstack.user" : username,
+	"browserstack.key" : accessKey
+};
 
 var driver = new webdriver.Builder().
   usingServer("https://hub-cloud.browserstack.com/wd/hub").
   withCapabilities(capabilities).
   build();
 
-async function example() {
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("Jenkins",Key.RETURN);
-}
-example();
+var browserstack = require('browserstack-local');
+
+//creates an instance of Local
+var bs_local = new browserstack.Local();
+
+// replace <browserstack-accesskey> with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+var bs_local_args = { 'key': '<browserstack-accesskey>' };
+
+// starts the Local instance with the required arguments
+bs_local.start(bs_local_args, function() {
+  console.log("Started BrowserStackLocal");
+});
+
+// check if BrowserStack local instance is running
+console.log(bs_local.isRunning());
+
+// stop the Local instance
+bs_local.stop(function() {
+  console.log("Stopped BrowserStackLocal");
+});
